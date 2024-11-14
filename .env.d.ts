c430dd2c4c205c6a8c8a9ba7a7fe1a8eac09089c
Farnoush Daliran <farnoushfdf@gmail.com>
2024-11-14 13:19:00 +0100

    create awsTranslateService.ts

--- .env.d.ts
+++ .env.d.ts
@@ -1,24 +1,10 @@
 
-
-
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-
 declare const figma: PluginAPI; 
 
+declare type __html__ = string; 
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    __html__?: any;
-  }
-}
-
-
-type TextNode = any; 
+declare type TextNode = {
+  type: "TEXT"; 
+  characters: string; 
+  
+};
